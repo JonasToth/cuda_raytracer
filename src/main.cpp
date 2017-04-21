9dc87de394a5f2b4021cb5b6789e3c7d12b9d8ce
Jonas Toth <jonas.toth@gmail.com>
2017-04-22 00:31:42 +0200

    [Misc] test if buildbot worx correctly

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,8 @@
 
 #include <iostream>
 
-auto feature() { return 42; }
-
 int main(int  /*argc*/, char**  /*argv*/) 
 {
     std::cout << "Hello world, lets raytrace" << std::endl;
-    std::cout << feature() << std::endl;
     return 0;
 }
