e4afb9615439c0542a3cd67b8b4b1d7131e052db
Jonas Toth <jonas.toth@gmail.com>
2017-04-21 23:37:53 +0200

    [CMake] Fix building and started buildbot

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,11 @@
 
 #include <iostream>
 
+auto feature() { return 42; }
+
 int main(int  /*argc*/, char**  /*argv*/) 
 {
     std::cout << "Hello world, lets raytrace" << std::endl;
+    std::cout << feature() << std::endl;
     return 0;
 }
