fd8c63f56522a2cbb829d64bc0adaacc086a66a2
Jonas Toth <jonas.toth@gmail.com>
2017-04-23 19:30:45 +0200

    [Fix] silence gcc warning

--- test/test_ad_san.cpp
+++ test/test_ad_san.cpp
@@ -1,28 +1,25 @@
 // Testcase to load an object file
 
 #include "gtest/gtest.h"
+#include <iostream>
 
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 TEST(ad_san, test_out_of_bounds) {
     char some_array[10];
-#if defined (__GCC__)
-#   pragma GCC diagnostic push
-#   pragma GCC diagnostic ignored "-Wunused-but-set-variable"
-#endif
     some_array[9] = 0;
-#if defined (__GCC__)
-#   pragma GCC diagnostic pop
-#endif
 
 #if defined (__clang__)
-#   pragma clang diagnostic push
-#   pragma clang diagnostic ignored "-Warray-bounds"
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warray-bounds"
 #endif
     some_array[15] = 'c';
 #if defined (__clang__)
-#   pragma clang diagnostic pop
+#pragma clang diagnostic pop
 #endif
 }
+#pragma GCC diagnostic pop
 
 int main(int argc, char** argv)
 {
