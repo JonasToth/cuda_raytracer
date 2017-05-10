7a901a8066ffdb983b6a1caa8d63133acfefc726
Jonas Toth <jonas.toth@gmail.com>
2017-05-10 11:03:28 +0200

    [Misc] test better CI pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,6 @@
 int main(int  /*argc*/, char**  /*argv*/) 
 {
     std::cout << "Hello world, lets raytrace" << std::endl;
+    std::cout << "We do so much to test everything" << std::endl;
     return 0;
 }
