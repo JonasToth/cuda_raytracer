ed5a5515696106e02b62e2dd8c5ca72c1a46f647
Jonas Toth <jonas.toth@gmail.com>
2017-05-15 19:02:41 +0200

    [Fix] naming style

--- test/test_glfw.cpp
+++ test/test_glfw.cpp
@@ -1,22 +1,22 @@
 #include "gtest/gtest.h"
-#include <gsl/gsl>
 #include <GLFW/glfw3.h>
+#include <gsl/gsl>
 
 TEST(GLFW, init) {
-    auto init_val = glfwInit();
-    ASSERT_NE(init_val, 0) << "Could not initialize GLFW";
+    auto InitVal = glfwInit();
+    ASSERT_NE(InitVal, 0) << "Could not initialize GLFW";
     glfwTerminate();
 }
 
 TEST(GLFW, window) {
-    auto init_val = glfwInit();
-    ASSERT_NE(init_val, 0) << "Could not initialize GLFW";
+    auto InitVal = glfwInit();
+    ASSERT_NE(InitVal, 0) << "Could not initialize GLFW";
 
-    gsl::owner<GLFWwindow*> window = glfwCreateWindow(640, 480, "Test", nullptr, nullptr);
-    ASSERT_NE(window, nullptr) << "Window not created";
+    gsl::owner<GLFWwindow*> Window = glfwCreateWindow(640, 480, "Test", nullptr, nullptr);
+    ASSERT_NE(Window, nullptr) << "Window not created";
     
     // cleanup
-    glfwDestroyWindow(window);
+    glfwDestroyWindow(Window);
     glfwTerminate();
 }
 
