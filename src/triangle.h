3c395962439afbcc1b2aa465f38b66a7e7695398
Jonas Toth <jonas.toth@gmail.com>
2017-05-31 18:58:47 +0200

    [Geometry] start file for triangles

---
+++ src/triangle.h
@@ -0,0 +1,17 @@
+#ifndef TRIANGLE_H_ESIZ1HBQ
+#define TRIANGLE_H_ESIZ1HBQ
+
+
+/** @file src/triangle.h
+ * Implement the possibility to create triangles, that must be combined in a mesh/world.
+ */
+
+
+
+/// A triangle is a set of 3 points, the order of the points defines the orientation.
+class triangle {
+
+};
+
+
+#endif /* end of include guard: TRIANGLE_H_ESIZ1HBQ */
