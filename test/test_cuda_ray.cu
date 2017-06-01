87365555f6e52b522ae4ecdf5892005fa8ef4729
Jonas Toth <jonas.toth@gmail.com>
2017-06-01 12:33:49 +0200

    [Fix] naming

--- test/test_cuda_ray.cu
+++ test/test_cuda_ray.cu
@@ -3,9 +3,9 @@
 
 TEST(cuda_ray, init)
 {
-    ray r;
-    r.origin = coord{0, 0, -15};
-    r.direction = coord{0, 0, 1};
+    ray R;
+    R.origin = coord{0, 0, -15};
+    R.direction = coord{0, 0, 1};
 }
 
 int main(int argc, char** argv) {
--- test/test_ray.cpp
+++ test/test_ray.cpp
@@ -3,9 +3,9 @@
 
 TEST(ray, init)
 {
-    ray r;
-    r.origin = coord{0, 0, -15};
-    r.direction = coord{0, 0, 1};
+    ray R;
+    R.origin = coord{0, 0, -15};
+    R.direction = coord{0, 0, 1};
 }
 
 int main(int argc, char** argv) {
