7459c77f844c01d19be54cdf22c858b785b4a0c9
Jonas Toth <jonas.toth@gmail.com>
2017-06-05 13:31:09 +0200

    [Ray] perspective triangle works

--- test/test_cuda_ray.cu
+++ test/test_cuda_ray.cu
@@ -13,7 +13,6 @@
 
 #include "ray.h"
 
-constexpr std::size_t SquareDim = 10;
 
 struct does_intersect {
     CUCALL bool operator()(const LIB::pair<bool, intersect>& r) { return r.first; };
@@ -117,6 +116,7 @@ std::string bwOutput(const thrust::device_vector<LIB::pair<bool, intersect>>& Re
 
 TEST(ray, trace_many_successfull)
 {
+    constexpr std::size_t SquareDim = 10;
     thrust::device_vector<coord> Vertices(3);
     Vertices[0] = {0,-1,1}; 
     Vertices[1] = {-1,1,1};
@@ -162,6 +162,7 @@ TEST(ray, trace_many_successfull)
 
 TEST(ray, trace_many_failing)
 {
+    constexpr std::size_t SquareDim = 10;
     thrust::device_vector<coord> Vertices(3);
     Vertices[0] = {0,-1,1}; 
     Vertices[1] = {-1,1,1};
@@ -179,9 +180,56 @@ TEST(ray, trace_many_failing)
     const auto AllRays = generateRays(&Origin, SquareDim);
     const auto Result = traceTriangle(T, AllRays);
     
+    ASSERT_EQ(LIB::none_of(thrust::device, Result.begin(), Result.end(), does_intersect{}),
+              true) << bwOutput(Result, SquareDim);
+
     std::cout << bwOutput(Result, SquareDim) << std::endl;
-    const auto ContainsHit = LIB::any_of(Result.begin(), Result.end(), does_intersect{});
-    ASSERT_EQ(ContainsHit, false) << bwOutput(Result, SquareDim);
+    OUT << "BW output done" << std::endl;
+}
+
+TEST(ray, trace_perspective)
+{
+    constexpr std::size_t SquareDim = 30;
+    thrust::device_vector<coord> Vertices(3);
+    Vertices[0] = {.5f,-1,1}; 
+    Vertices[1] = {-1,.5f,1};
+    Vertices[2] = {1,1,1};
+    const thrust::device_ptr<coord> P0 = &Vertices[0];
+    const thrust::device_ptr<coord> P1 = &Vertices[1];
+    const thrust::device_ptr<coord> P2 = &Vertices[2];
+
+    const coord Origin{0, 0, 0};
+
+    const auto triangle_void = thrust::device_malloc(sizeof(triangle));
+    auto _ = gsl::finally([&triangle_void]() { thrust::device_free(triangle_void); });
+    const auto triangle_ptr = thrust::device_new(triangle_void, triangle{P0.get(), P1.get(), P2.get()});
+
+    OUT << "Triangle and tracer origin created" << std::endl;
+
+    const auto AllRays = generateRays(&Origin, SquareDim);
+    ASSERT_EQ(AllRays.size(), SquareDim * SquareDim);
+    OUT << "Rays generated" << std::endl;
+    const auto Result = traceTriangle(triangle_ptr, AllRays);
+    ASSERT_EQ(Result.size(), SquareDim * SquareDim);
+    OUT << "Raytracing done" << std::endl;
+
+    const auto ContainsHit = LIB::any_of(thrust::device, Result.begin(), Result.end(), 
+                                         does_intersect{});
+    ASSERT_EQ(ContainsHit, true) << bwOutput(Result, SquareDim);
+
+    const auto GoodDepth = LIB::all_of(thrust::device, Result.begin(), Result.end(),  
+                                       has_good_depth{});
+    ASSERT_EQ(GoodDepth, true) << bwOutput(Result, SquareDim);
+
+    const auto HitCount = LIB::count_if(thrust::device, Result.begin(), Result.end(), 
+                                        does_intersect{});
+    ASSERT_GT(HitCount, 0.1 * SquareDim * SquareDim) << bwOutput(Result, SquareDim) +
+                                                        "More hits are expected\n";
+    ASSERT_LT(HitCount, 0.5 * SquareDim * SquareDim) << bwOutput(Result, SquareDim) +
+                                                        "Less hits are expected\n";
+
+    std::cout << bwOutput(Result, SquareDim) << std::endl;
+    OUT << "BW output done" << std::endl;
 }
 
 int main(int argc, char** argv) {
