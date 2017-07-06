7b014b293d1baa0279d717de152cac47463ecc38
Jonas Toth <jonas.toth@gmail.com>
2017-07-06 16:11:20 +0200

    [Feature] basic shading coeffs implemented

--- src/graphic/shading.h
+++ src/graphic/shading.h
@@ -5,7 +5,32 @@
 #include "graphic/light.h"
 #include "macros.h"
 
+#ifndef __CUDACC__
+#   include <gsl/gsl>
+#endif
+
+/// Calculate ambient lighting from the global coefficient and the material coefficient
 CUCALL ALWAYS_INLINE inline float ambient(float ka, float ia) noexcept { return ka * ia; }
+
+/// Calculate diffuse lighting from material and light coeffs + Surface Normal(N), Light direction (L)
+CUCALL ALWAYS_INLINE inline float diffuse(float kd, float id, coord N, coord L)
+{
+    // GSL not in Cuda, since no exceptions possible
+#ifndef __CUDACC__
+    Expects(norm(N) - 1.f < 0.0001f);
+    Expects(norm(L) - 1.f < 0.0001f);
+#endif
+
+    return kd * dot(L, N) * id;
+}
+
+/// Calculate specular reflection depending on material, light, direction of camera(V)
+/// and direction of reflection ray (R) and shininess alpha
+CUCALL ALWAYS_INLINE inline float specular(float ks, float is, coord V, coord R, float alpha)
+{
+    return ks * std::pow(dot(R, V), alpha) * is;
+}
+
 
 
 #endif /* end of include guard: SHADING_H_7ITEXQWS */
--- test/test_shading.cpp
+++ test/test_shading.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 
 #include "graphic/shading.h"
+#include "graphic/vector.h"
 
 
 TEST(shading, ambient_coeff)
@@ -8,17 +9,94 @@ TEST(shading, ambient_coeff)
     EXPECT_FLOAT_EQ(ambient(0.f, 0.f), 0.f) << "Expected no ambient";
     EXPECT_FLOAT_EQ(ambient(0.f, 1.f), 0.f) << "Expected no ambient";
     EXPECT_FLOAT_EQ(ambient(1.f, 0.f), 0.f) << "Expected no ambient";
-    EXPECT_FLOAT_EQ(ambient(1.f, 1.f), 1.f) << "Expected ambient 1";
+
+    const auto ExpectedOk = ambient(1.f, 1.f);
+    EXPECT_FLOAT_EQ(ExpectedOk, 1.f) << "Expected ambient 1";
+    OUT << "Good Value: " << ExpectedOk << std::endl;
 }
 
 TEST(shading, diffuse_coeff)
 {
+    const coord XAxis(1.f, 0.f, 0.f);
+    const coord YAxis(0.f, 1.f, 0.f);
+    const coord ZAxis(0.f, 0.f, 1.f);
 
+    const coord RealDirection1(normalize(coord(10.f, 20.f, -4.f)));
+    const coord RealDirection2(normalize(coord(2.f, 1.f, -6.f)));
+
+    const float MatNone = 0.f;
+    const float LightNone = 0.f;
+
+    const float MatGud = 0.5f;
+    const float LightGud = 1.6f;
+
+    // Playing with the material coefficients
+    EXPECT_FLOAT_EQ(diffuse(MatNone, LightNone, RealDirection1, RealDirection2), 0.f) 
+                    << "No diffuse material and light should return 0.f";
+    EXPECT_FLOAT_EQ(diffuse(MatGud, LightNone, RealDirection1, RealDirection2), 0.f) 
+                    << "No diffuse light should return 0.f";
+    EXPECT_FLOAT_EQ(diffuse(MatNone, LightGud, RealDirection1, RealDirection2), 0.f) 
+                    << "No diffuse material should return 0.f";
+    const auto ExpectedOk = diffuse(MatGud, LightGud, RealDirection1, RealDirection2);
+    EXPECT_NE(ExpectedOk, 0.f) 
+              << "diffuse material and light should return !=0.f";
+    OUT << "Good Value: " << ExpectedOk << std::endl;
+
+    // Playing with light source and camera direction
+    EXPECT_FLOAT_EQ(diffuse(MatGud, LightGud, XAxis, YAxis), 0.f)
+                    << "Orthogonal directions should be zero";
+    EXPECT_FLOAT_EQ(diffuse(MatGud, LightGud, XAxis, ZAxis), 0.f)
+                    << "Orthogonal directions should be zero";
+    EXPECT_FLOAT_EQ(diffuse(MatGud, LightGud, YAxis, ZAxis), 0.f)
+                    << "Orthogonal directions should be zero";
+
+    EXPECT_NE(diffuse(MatGud, LightGud, RealDirection1, ZAxis), 0.f)
+                    << "Valid Directions result in nonzero diffuse";
 }
 
 TEST(shading, specular_coeff)
 {
+    const coord XAxis(1.f, 0.f, 0.f);
+    const coord YAxis(0.f, 1.f, 0.f);
+    const coord ZAxis(0.f, 0.f, 1.f);
 
+    const coord RealDirection1(normalize(coord(10.f, 20.f, -4.f)));
+    const coord RealDirection2(normalize(coord(2.f, 1.f, -6.f)));
+
+    const float MatNone   = 0.f;
+    const float LightNone = 0.f;
+
+    const float MatGud    = 0.5f;
+    const float LightGud  = 1.6f;
+
+    const float ShineNone = 0.f;
+    const float ShineGud  = 2.f;
+
+    // playing with material coefficients
+    EXPECT_FLOAT_EQ(specular(MatNone, LightNone, RealDirection1, RealDirection2, ShineNone), 0.f)
+                    << "No material, light and shininess needs to result in 0";
+    EXPECT_FLOAT_EQ(specular(MatNone, LightNone, RealDirection1, RealDirection2, ShineGud), 0.f)
+                    << "No material, light needs to result in 0";
+    EXPECT_FLOAT_EQ(specular(MatNone, LightGud, RealDirection1, RealDirection2, ShineNone), 0.f)
+                    << "No material and shininess needs to result in 0";
+    EXPECT_FLOAT_EQ(specular(MatNone, LightGud, RealDirection1, RealDirection2, ShineGud), 0.f)
+                    << "No material needs to result in 0";
+    EXPECT_FLOAT_EQ(specular(MatGud, LightNone, RealDirection1, RealDirection2, ShineNone), 0.f)
+                    << "No light and shininess needs to result in 0";
+    EXPECT_FLOAT_EQ(specular(MatGud, LightNone, RealDirection1, RealDirection2, ShineGud), 0.f)
+                    << "No light needs to result in 0";
+    const auto ExpectedOk = specular(MatGud, LightGud, RealDirection1, RealDirection2, ShineGud);
+    EXPECT_NE(ExpectedOk, 0.f) << "Good Value Expected";
+    OUT << "Good Value: " << ExpectedOk << std::endl;
+
+    // playing with directions
+    EXPECT_FLOAT_EQ(specular(MatGud, LightNone, XAxis, ZAxis, ShineGud), 0.f)
+                    << "Orthogonal directions result in no specular";
+    EXPECT_FLOAT_EQ(specular(MatGud, LightNone, YAxis, ZAxis, ShineGud), 0.f)
+                    << "Orthogonal directions result in no specular";
+
+    EXPECT_NE(specular(MatGud, LightGud, RealDirection1, ZAxis, ShineGud), 0.f)
+              << "Valid directions result in nonzero";
 }
 
 int main(int argc, char** argv)
