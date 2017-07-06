845276d29a40c5a8f92d93411a7ef7dde3584991
Jonas Toth <jonas.toth@gmail.com>
2017-07-06 18:58:18 +0200

    [Feature] implement registering callbacks for input

---
+++ src/management/input_callback.cpp
@@ -0,0 +1,20 @@
+#include "management/input_callbacks.h"
+#include "management/input_manager.h"
+#include <GLFW/glfw3.h>
+
+void register_key_press(GLFWwindow* w, int key, int scancode, int action, int mods)
+{
+    auto& im = input_manager::instance();
+
+    if(action == GLFW_PRESS)
+        im.press(key);
+    else if(action == GLFW_RELEASE)
+        im.release(key);
+}
+
+
+void register_mouse_movement(GLFWwindow* w, double xpos, double ypos)
+{
+    auto& im = input_manager::instance();
+	im.move_mouse(xpos, ypos);
+}
---
+++ src/management/input_callback.h
@@ -0,0 +1,14 @@
+#ifndef INPUT_CALLBACK_H_TIZFXPK4
+#define INPUT_CALLBACK_H_TIZFXPK4
+
+/// fwd declare, to reduce memory IO
+struct GLFWwindow;
+
+/// Callback for glfw to handle key events, that are simply registered in input_manager
+void register_key_press(GLFWwindow* w, int key, int scancode, int action, int mods);
+
+/// Callback for glfw to handle mouse movement, that are simply registered in input_manager
+void register_mouse_movement(GLFWwindow* w, double xpos, double ypos);
+
+
+#endif /* end of include guard: INPUT_CALLBACK_H_TIZFXPK4 */
