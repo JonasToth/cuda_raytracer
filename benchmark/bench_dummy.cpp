fe51c0743c9539186838dd29277684b778235dba
Jonas Toth <jonas.toth@gmail.com>
2017-07-09 13:11:21 +0200

    [Benchmark] first dummy benchmark works

--- benchmark/bench_dummy.cpp
+++ benchmark/bench_dummy.cpp
@@ -1,4 +1,4 @@
-#include "benchmark/benchmark_api.h"
+#include "benchmark/benchmark.h"
 #include <vector>
 
 static void BM_VectorInsert(benchmark::State& state)
@@ -6,11 +6,11 @@ static void BM_VectorInsert(benchmark::State& state)
     while(state.KeepRunning())
     {
         std::vector<int> insertion_test;
-        for(int i = 0, i_end = state.range_x(); i < i_end; ++i)
+        for(int i = 0, i_end = state.range(0); i < i_end; ++i)
             insertion_test.push_back(i);
     }
 }
 
 BENCHMARK(BM_VectorInsert)->Range(8,8 << 10);
 
-BENCHMARK_MAIN();
+BENCHMARK_MAIN()
