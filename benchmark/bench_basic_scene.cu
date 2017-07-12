12fed03adc43f047a11873bfab237c2c0e0bc866
Jonas Toth <jonas.toth@gmail.com>
2017-07-12 10:49:53 +0200

    [Fix] benchmark works now as well

--- benchmark/bench_basic_scene.cu
+++ benchmark/bench_basic_scene.cu
@@ -42,10 +42,10 @@ static void BM_SceneRender(benchmark::State& state)
     float ambi[3] = {0.2f, 0.2f, 0.2f};
     float no_ambi[3] = {0.01f, 0.01f, 0.01f};
     thrust::device_vector<light_source> lights;
-    lights.push_back({{spec, diff, ambi}, {0.8f, 0.9f, 1.5f}});
-    lights.push_back({{spec, diff, no_ambi}, {1.7f, -1.1f, -0.3f}});
-    lights.push_back({{spec, diff, no_ambi}, {-1.3f, 0.8f, 2.0f}});
-    lights.push_back({{spec, diff, no_ambi}, {-1.7f, -1.7f, 0.8f}});
+    lights.push_back({phong_light(spec, diff, ambi), {0.8f, 0.9f, 1.5f}});
+    lights.push_back({phong_light(spec, diff, no_ambi), {1.7f, -1.1f, -0.3f}});
+    lights.push_back({phong_light(spec, diff, no_ambi), {-1.3f, 0.8f, 2.0f}});
+    lights.push_back({phong_light(spec, diff, no_ambi), {-1.7f, -1.7f, 0.8f}});
 
     const auto& triangles = scene.triangles();
 
