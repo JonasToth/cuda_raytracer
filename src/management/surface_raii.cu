87c337840cb9f3dd96222ef1aaa4da642b3ab919
Jonas Toth <jonas.toth@gmail.com>
2017-07-15 13:15:40 +0200

    [Misc] working on surface memory handling

--- src/management/surface_raii.cu
+++ src/management/surface_raii.cu
@@ -1,6 +1,7 @@
 #include "surface_raii.h"
 
 #include <chrono>
+#include <gsl/gsl>
 #include <iostream>
 
 #pragma diagnostic push
@@ -10,37 +11,34 @@
 #include <stdexcept>
 #include <thread>
 
-
-surface_raii::surface_raii(int width, int height)
-  : __width{width}
+surface_raii::surface_raii(int width, int height, render_target target)
+  : __target{target}
+  , __width{width}
   , __height{height}
   , __texture{0}
-  , __cuda_array{}
-  , __cuda_array_resource_desc{}
-  , __cuda_resource{}
-  , __cuda_surface{}
 {
-    __initialize_texture();
+    __initialize_render_target();
     __initialize_cuda_surface();
 }
 
 surface_raii::~surface_raii()
 {
-    // Destroy the opengl texture
-    glDeleteTextures(1, &__texture);
+    if (__target == render_target::texture) {
+        // Destroy the opengl texture
+        glDeleteTextures(1, &__texture);
+        // Destroy link between cuda and opengl
+        cudaGraphicsUnmapResources(1, &__cuda_resource);
+    }
 
-    // Destroy all cuda and opengl connections
     cudaDestroySurfaceObject(__cuda_surface);
-    cudaGraphicsUnmapResources(1, &__cuda_resource);
 }
-
 
 namespace
 {
 png::image<png::rgba_pixel> memory_to_png(const std::vector<uint8_t>& memory,
-                                          std::size_t width, std::size_t height)
+                                          std::size_t width, std::size_t height,
+                                          const int channels)
 {
-    const auto channels = 4;
     png::image<png::rgba_pixel> img(width, height);
     for (std::size_t y = 0ul; y < height; ++y) {
         for (std::size_t x = 0ul; x < width; ++x) {
@@ -58,13 +56,24 @@ void surface_raii::save_as_png(const std::string& file_name) const
 {
     const auto memory = __get_texture_memory();
     // const not allowed, IDK why
-    auto img = memory_to_png(memory, __width, __height);
+    auto img = memory_to_png(memory, __width, __height, __channels);
     img.write(file_name);
 }
 
+void surface_raii::__initialize_render_target()
+{
+    // https://stackoverflow.com/questions/19244191/
+    // cuda-opengl-interop-draw-to-opengl-texture-with-cuda
+    if (__target == render_target::texture)
+        __initialize_opengl_texture();
+    else if (__target == render_target::memory)
+        __initialize_memory_texture();
+    else
+        throw std::logic_error{"Unexcpected value for render_target!"};
+}
 
-// https://stackoverflow.com/questions/19244191/cuda-opengl-interop-draw-to-opengl-texture-with-cuda
-void surface_raii::__initialize_texture()
+
+void surface_raii::__initialize_opengl_texture()
 {
     glEnable(GL_TEXTURE_2D);
     glGenTextures(1, &__texture);
@@ -74,6 +83,7 @@ void surface_raii::__initialize_texture()
 
     glBindTexture(GL_TEXTURE_2D, __texture);
     { // beauty stuff for opengl, maybe skip?
+        Expects(__channels == 4);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, __width, __height, 0, GL_RGBA,
@@ -102,20 +112,25 @@ void surface_raii::__initialize_texture()
     cudaGraphicsMapResources(1, &__cuda_resource);
 }
 
+void surface_raii::__initialize_memory_texture()
+{
+    // Allocate memory in RAM
+    __memory_texture.resize(__channels * __width * __height);
+}
 
 void surface_raii::__initialize_cuda_surface()
 {
     // source: Internet :)
-    // https://stackoverflow.com/questions/19244191/cuda-opengl-interop-draw-to-opengl-texture-with-cuda
+    // https://stackoverflow.com/questions/19244191/
+    // cuda-opengl-interop-draw-to-opengl-texture-with-cuda
+    // http://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#surface-object-api
     cudaGraphicsSubResourceGetMappedArray(&__cuda_array, __cuda_resource, 0, 0);
-
     __cuda_array_resource_desc.resType = cudaResourceTypeArray;
     __cuda_array_resource_desc.res.array.array = __cuda_array;
 
     // Surface creation
     cudaCreateSurfaceObject(&__cuda_surface, &__cuda_array_resource_desc);
 }
-
 
 void surface_raii::render_gl_texture() noexcept
 {
@@ -138,16 +153,18 @@ void surface_raii::render_gl_texture() noexcept
     glFinish();
 }
 
-
 std::vector<uint8_t> surface_raii::__get_texture_memory() const
 {
-    const auto channels = 4;
-    std::vector<uint8_t> gl_texture_data(__width * __height * channels);
-    glReadPixels(0, 0, __width, __height, GL_RGBA, GL_UNSIGNED_BYTE,
-                 gl_texture_data.data());
+    if (__target == render_target::texture) {
+        std::vector<uint8_t> gl_texture_data(__width * __height * __channels);
+        glReadPixels(0, 0, __width, __height, GL_RGBA, GL_UNSIGNED_BYTE,
+                     gl_texture_data.data());
 
-    return gl_texture_data;
+        return gl_texture_data;
+    } else if (__target == render_target::memory)
+        return __memory_texture;
+    else
+        throw std::logic_error{"Unexpected render target"};
 }
-
 
 #pragma GCC diagnostic pop
--- src/management/surface_raii.h
+++ src/management/surface_raii.h
@@ -7,11 +7,16 @@
 #include <string>
 #include <vector>
 
+enum class render_target {
+    texture, ///< render to an opengl texture, visualizable
+    memory,  ///< render to plain memory, just output to image
+};
+
 /// Controls the output to the screen
 class surface_raii
 {
 public:
-    surface_raii(int width, int height);
+    surface_raii(int width, int height, render_target target = render_target::texture);
     ~surface_raii();
 
     cudaSurfaceObject_t& getSurface() noexcept { return __cuda_surface; }
@@ -20,16 +25,27 @@ public:
     void save_as_png(const std::string& file_name) const;
 
 private:
-    void __initialize_texture();
+    /// Decide if opengl or raw memory
+    void __initialize_render_target();
+    /// Handle opengl
+    void __initialize_opengl_texture();
+    /// Handle raw memory
+    void __initialize_memory_texture();
+    /// Create CudaSurface from opengl
     void __initialize_cuda_surface();
 
+    /// Helper to extract memory from which resource ever
     std::vector<uint8_t> __get_texture_memory() const;
 
-    int __width;
-    int __height;
+    const int __channels = 4;    ///< RGBA constant
+    enum render_target __target; ///< flag for opengl or memory
+    int __width;                 ///< width of the texture
+    int __height;                ///< height of the texture
 
-    GLuint __texture;
+    std::vector<uint8_t> __memory_texture; ///< when rendering to plain memory, buffer
+    GLuint __texture; ///< when using an opengl texture, this value will be set
 
+    /// When using an gpu, this will be used to handle the memory there
     cudaArray_t __cuda_array;
     cudaResourceDesc __cuda_array_resource_desc;
     cudaGraphicsResource_t __cuda_resource;
