e9d989beec8de078eb75fe1ebdf11b5177919b40
Jonas Toth <jonas.toth@gmail.com>
2017-07-17 16:31:26 +0200

    [Misc] refactor

--- src/graphic/render/world_depth.h
+++ src/graphic/render/world_depth.h
@@ -5,16 +5,35 @@
 #include "graphic/kernels/utility.h"
 
 #ifdef __CUDACC__
-void raytrace_many_cuda(cudaSurfaceObject_t Surface, const camera& c,
-                        const triangle* Triangles, int TriangleCount);
-void raytrace_cuda(cudaSurfaceObject_t& Surface, int width, int height, const triangle* T);
+inline void raytrace_many_cuda(cudaSurfaceObject_t Surface, const camera& c,
+                               gsl::span<const triangle> triangles)
+{
+    dim3 dimBlock(32, 32);
+    dim3 dimGrid((c.width() + dimBlock.x) / dimBlock.x,
+                 (c.height() + dimBlock.y) / dimBlock.y);
+    black_kernel<<<dimGrid, dimBlock>>>(Surface, c.width(), c.height());
+    trace_many_triangles_with_camera<<<dimGrid, dimBlock>>>(
+        Surface, c, triangles, triangles.size(), c.width(), c.height());
+}
+
+
+inline void raytrace_cuda(cudaSurfaceObject_t& Surface, int width, int height,
+                          const triangle* T)
+{
+    dim3 dimBlock(32, 32);
+    dim3 dimGrid((width + dimBlock.x) / dimBlock.x, (height + dimBlock.y) / dimBlock.y);
+    trace_single_triangle<<<dimGrid, dimBlock>>>(Surface, T, width, height);
+}
+
+
 #include "util/kernel_launcher/world_depth.inl"
 
 #else
+
 inline void raytrace_many_cuda(memory_surface& s, const camera& c,
-                               const triangle* triangles, std::size_t triangle_count)
+                               gsl::span<const triangle> triangles)
 {
-    trace_many_triangles_with_camera(s, c, triangles, triangle_count);
+    trace_many_triangles_with_camera(s, c, triangles, triangles.size());
 }
 
 inline void raytrace_cuda(memory_surface& s, const triangle& t)
